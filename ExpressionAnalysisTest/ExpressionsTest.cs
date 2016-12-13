f5b40f084560d7e289ca36319dde06b314c82dae
zoyeal <zoyeal@hotmail.com>
2016-12-13 10:40:06 +0800

    v2

--- ExpressionAnalysisTest/ExpressionsTest.cs
+++ ExpressionAnalysisTest/ExpressionsTest.cs
@@ -6,14 +6,16 @@ namespace ExpressionAnalysisTest
     [TestFixture]
     public class ExpressionsTest
     {
-        private const string LongExpression = "8+((11+1)*2-(3-2)*2)/2+1.5";
+        private const string LongExpression = "8+((11+1)*2-(3-2)*2)/2+1";
+        private const string ShortExpression = "6*(3+5)-4*7";
 
         [Test]
         [TestCase(LongExpression)]
+        [TestCase(ShortExpression)]
         public void CalculationTest(string str)
         {
             var result = new Expressions().Expression(str);
-            Assert.AreEqual(20.5 , result);
+            Assert.AreEqual(20 , result);
         }
     }
 }
